8ad12e88f71df65edc13003f68d165421c18cd59
ryandgoldenberg1 <ryandgoldenberg1@gmail.com>
2015-09-05 00:50:36 +0000

    Create games controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :games, only: [:new, :create]
+  get 'play' => 'games#new'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
-  root 'devise/sessions#new'
+  root 'games#new'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
